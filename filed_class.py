03d94d9ebe5fa406777dddfdc60e069ecda3551c
uma <chiderovauma@gmail.com>
2024-01-24 17:07:14 +0600

    Added type hints for all methods of the Field() and Player() classes.

--- filed_class.py
+++ filed_class.py
@@ -7,7 +7,7 @@ from helpers.variables import empty_field_vertical_line_template, \
 
 
 class Field:
-    def __init__(self, size):
+    def __init__(self, size: int) -> None:
         self.size = size
         self.last_col = size - 1
         self.percent_of_size_num = percent_nums_data.get(size)
@@ -16,7 +16,7 @@ class Field:
         self.filled_cells = {X_SYMBOL: [], O_SYMBOL: []}
 
 
-    def add_user_coords(self, symbol: str, coords: list) -> str or None:
+    def add_user_coords(self, symbol: str, coords: str) -> str or None:
         user_coords = [int(data) for data in coords if data.isdigit()]
         if len(user_coords) != 2:
             return ENTER_CORRECT_COORDS_MESSAGE
@@ -42,11 +42,11 @@ class Field:
         self.field = f"{header_text}\n{horizontal_text}"
 
     @staticmethod
-    def create_header(col_number):
+    def create_header(col_number: int) -> str:
         header_indentation_num = 5 if col_number < 3 else 6
         return f"{' ' * header_indentation_num}{col_number}"
 
-    def create_vertical_lines(self, symbol, row_num) -> str:
+    def create_vertical_lines(self, symbol: str, row_num: int) -> str:
         vertical_lines = ''
         for col in range(0, self.size):
             for coords_data in self.filled_cells.get(symbol, []):
@@ -69,5 +69,5 @@ class Field:
         horizontal_lines_count = 0 if row_num == (self.size - 1) else self.horizontal_lines_count
         return '--' * horizontal_lines_count
 
-    def __str__(self):
+    def __str__(self) -> None:
         print(self.field)
--- player_class.py
+++ player_class.py
@@ -1,14 +1,11 @@
 from filed_class import Field
-from helpers.info_messages import ENTER_FIELD_SIZE_MESSAGE, \
-    ENTER_COORDS_MESSAGE, \
-    CHOOSE_SYMBOL_MESSAGE, \
-    CHOOSE_CORRECT_SYMBOL_MESSAGE
-from helpers.secondary_functions import user_input
+from helpers.info_messages import ENTER_COORDS_MESSAGE, CHOOSE_SYMBOL_MESSAGE, CHOOSE_CORRECT_SYMBOL_MESSAGE, \
+    SYMBOL_IS_OCCUPIED_MESSAGE
 from helpers.variables import X_SYMBOL, O_SYMBOL
 
 
 class Player:
-    def __init__(self, filed_obj):
+    def __init__(self, filed_obj: Field):
         self.field = filed_obj
         self.symbol = None
 
@@ -22,7 +19,7 @@ class Player:
             message = CHOOSE_CORRECT_SYMBOL_MESSAGE
             continue
 
-    def step(self):
+    def step(self) -> None:
         message = ENTER_COORDS_MESSAGE
         while True:
             user_coords = input(message)
@@ -31,17 +28,6 @@ class Player:
                 message = result
                 continue
             else:
-                message = ENTER_COORDS_MESSAGE
                 self.field.create_field(symbol=self.symbol)
                 self.field.__str__()
-
-
-if __name__ == "__main__":
-    field_size = user_input(message=ENTER_FIELD_SIZE_MESSAGE)
-    field = Field(size=field_size)
-    field.create_field()
-    field.__str__()
-    print("Start game")
-    player = Player(filed_obj=field)
-    player.choose_symbol()
-    player.step()
+                break
