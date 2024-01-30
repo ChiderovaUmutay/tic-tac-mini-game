339ac1e4e481b18cc252c8c90eef032e46b80956
uma <chiderovauma@gmail.com>
2024-01-30 14:49:08 +0600

    Created a new "Bot()" class which inherited from "Player()" class.

---
+++ bot_class.py
@@ -0,0 +1,27 @@
+from random import randint
+
+from player_class import Player
+
+
+class Bot(Player):
+
+    def step(self):
+        last_col = self.field.last_col
+        while True:
+            coords = self.get_bot_coords(last_col_num=last_col)
+            result = self.field.add_user_coords(symbol=self.symbol, coords=coords)
+            if not isinstance(result, str):
+                self.field.create_field(symbol=self.symbol)
+                self.display(coords=coords)
+                self.field.__str__()
+                break
+
+    @staticmethod
+    def get_bot_coords(last_col_num) -> str:
+        col = randint(0, last_col_num)
+        row = randint(0, last_col_num)
+        return f"{col} {row}"
+
+    @staticmethod
+    def display(coords: str) -> None:
+        print(coords)
