942d5ede8edde07f159e70bd4a76a26530ccf99f
uma <chiderovauma@gmail.com>
2024-05-31 11:39:48 +0600

    Removed the condition, which runs all game.

--- bot_class.py
+++ bot_class.py
@@ -1,9 +1,5 @@
 from random import randint
 
-from checker_class import Checker
-from filed_class import Field
-from helpers.info_messages import ENTER_FIELD_SIZE_MESSAGE
-from helpers.secondary_functions import user_input
 from player_class import Player
 
 
@@ -21,7 +17,7 @@ class Bot(Player):
                 break
 
     @staticmethod
-    def get_bot_coords(last_col_num) -> str:
+    def get_bot_coords(last_col_num: int) -> str:
         col = randint(0, last_col_num)
         row = randint(0, last_col_num)
         return f"{col} {row}"
@@ -29,27 +25,3 @@ class Bot(Player):
     @staticmethod
     def display(coords: str) -> None:
         print(coords)
-
-
-if __name__ == "__main__":
-    field_size = user_input(message=ENTER_FIELD_SIZE_MESSAGE)
-    field = Field(size=field_size)
-    field.create_field()
-    field.__str__()
-    player = Player(filed_obj=field)
-    player.choose_symbol()
-    bot = Bot(filed_obj=field)
-    bot.choose_symbol(bot=True)
-    checker = Checker(field)
-    checker.get_winning_combination()
-    while True:
-        player.step()
-        result = checker.checker(player=player)
-        if result:
-            print("User win")
-            break
-        bot.step()
-        result = checker.checker(player=bot)
-        if result:
-            print("Bot win")
-            break
